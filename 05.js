22f9cdb86e3fdaab177602ca76e0dcfc713cde55
UrePu <darkin31313@gmail.com>
2025-01-13 17:58:49 +0900

    5

--- 05.js
+++ 05.js
@@ -13,7 +13,14 @@
  * @returns {string[]}
  */
 
-function filterUserTodos(userTodos, userId, priority) {}
+function filterUserTodos(userTodos, userId, priority) {
+  return userTodos
+    .filter(
+      (e) =>
+        e.userId === userId && e.priority === priority && e.completed === false
+    )
+    .map((d) => d.title);
+}
 
 // export 수정 불가
 export { filterUserTodos };
